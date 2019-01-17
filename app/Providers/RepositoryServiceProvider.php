272f45688fafe9725327e436b998b94debaf3377
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-17 11:45:14 -0200

    resolvendo interfaces/

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -23,14 +23,15 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(\StorePoll\Repositories\CategoryRepository::class, \StorePoll\Repositories\CategoryRepositoryEloquent::class);
-        $this->app->bind(\StorePoll\Repositories\ProductRepository::class, \StorePoll\Repositories\ProductRepositoryEloquent::class);
-        $this->app->bind(\StorePoll\Repositories\TagRepository::class, \StorePoll\Repositories\TagRepositoryEloquent::class);
-        $this->app->bind(\StorePoll\Repositories\SaleOrderRepository::class, \StorePoll\Repositories\SaleOrderRepositoryEloquent::class);
-        $this->app->bind(\StorePoll\Repositories\SaleLineRepository::class, \StorePoll\Repositories\SaleLineRepositoryEloquent::class);
-        $this->app->bind(\StorePoll\Repositories\CustomerRepository::class, \StorePoll\Repositories\CustomerRepositoryEloquent::class);
-        $this->app->bind(\StorePoll\Repositories\UserRepository::class, \StorePoll\Repositories\UserRepositoryEloquent::class);
-        $this->app->bind(\StorePoll\Repositories\AddressRepository::class, \StorePoll\Repositories\AddressRepositoryEloquent::class);
+        $this->app->bind(\StorePoll\Repositories\Interfaces\CategoryRepository::class, \StorePoll\Repositories\CategoryRepositoryEloquent::class);
+        $this->app->bind(\StorePoll\Repositories\Interfaces\ProductRepository::class, \StorePoll\Repositories\ProductRepositoryEloquent::class);
+        $this->app->bind(\StorePoll\Repositories\Interfaces\TagRepository::class, \StorePoll\Repositories\TagRepositoryEloquent::class);
+        $this->app->bind(\StorePoll\Repositories\Interfaces\SaleOrderRepository::class, \StorePoll\Repositories\SaleOrderRepositoryEloquent::class);
+        $this->app->bind(\StorePoll\Repositories\Interfaces\SaleLineRepository::class, \StorePoll\Repositories\SaleLineRepositoryEloquent::class);
+        $this->app->bind(\StorePoll\Repositories\Interfaces\CustomerRepository::class, \StorePoll\Repositories\CustomerRepositoryEloquent::class);
+        $this->app->bind(\StorePoll\Repositories\Interfaces\UserRepository::class, \StorePoll\Repositories\UserRepositoryEloquent::class);
+        $this->app->bind(\StorePoll\Repositories\Interfaces\AddressRepository::class, \StorePoll\Repositories\AddressRepositoryEloquent::class);
+        $this->app->bind(\StorePoll\Repositories\Interfaces\ContactRepository::class, \StorePoll\Repositories\ContactRepositoryEloquent::class);
         //:end-bindings:
     }
 }
