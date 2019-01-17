c80a575fc5762085ef5cd09031e883f4ba89446a
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-16 23:17:41 -0200

    iniciando cors da aplicação

---
+++ config/cors.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+   
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['http://127.0.0.1:8000',
+                         'http://localhost:8000',
+                         'http://localhost:9527',
+                         'ip-10-0-1-92.sa-east-1.compute.internal'
+                         ],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
