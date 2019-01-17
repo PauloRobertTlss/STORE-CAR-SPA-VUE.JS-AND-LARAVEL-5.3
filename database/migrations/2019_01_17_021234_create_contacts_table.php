2be3dbb4fa02d44109e5f450cea5036130c3c3d3
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-17 00:15:03 -0200

    tabela de contatos

---
+++ database/migrations/2019_01_17_021234_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateContactsTable.
+ */
+class CreateContactsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        schema::create('contacts', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('contact',20);
+            $table->integer('contactable_id')->unsigned();
+            $table->string('contactable_type');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('contacts');
+	}
+}
