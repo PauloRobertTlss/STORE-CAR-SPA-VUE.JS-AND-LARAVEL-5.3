851730d53f37dd256d83bcb71ff354f61544e289
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-17 21:53:55 -0200

    adicionando modulo Product

--- resources/assets/_store/js/store/index.js
+++ resources/assets/_store/js/store/index.js
@@ -4,18 +4,20 @@ import app from './modules/app'
 import errorLog from './modules/errorLog'
 import permission from './modules/permission'
 import tagsView from './modules/tagsView'
+import product from './modules/product'
 import getters from './getters'
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  modules: {
-    app,
-    errorLog,
-    permission,
-    tagsView
-  },
-  getters
+    modules: {
+        app,
+        errorLog,
+        permission,
+        tagsView,
+        product
+    },
+    getters
 });
 
 export default store
