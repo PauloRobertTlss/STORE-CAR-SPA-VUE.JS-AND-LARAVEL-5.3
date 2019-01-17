268efd79770ea86a8074f103dba197e9f9ad20e4
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-17 12:14:00 -0200

    adicionando getter errorLog

--- resources/assets/_store/js/store/getters.js
+++ resources/assets/_store/js/store/getters.js
@@ -1,9 +1,10 @@
 const getters = {
-  sidebar: state => state.app.sidebar,
-  language: state => state.app.language,
-  device: state => state.app.device,
-  visitedViews: state => state.tagsView.visitedViews,
-  cachedViews: state => state.tagsView.cachedViews,
-  addRouters: state => state.permission.addRouters
+    sidebar: state => state.app.sidebar,
+    language: state => state.app.language,
+    device: state => state.app.device,
+    visitedViews: state => state.tagsView.visitedViews,
+    cachedViews: state => state.tagsView.cachedViews,
+    addRouters: state => state.permission.addRouters,
+    errorLogs: state => state.errorLog.logs
 };
 export default getters
