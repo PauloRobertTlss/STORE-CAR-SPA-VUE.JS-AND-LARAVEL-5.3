06a9ebd2f31bf548bf18d6893d9f4f87e43860b2
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-17 20:40:31 -0200

    incluindo rotas de produtos e compras

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware'=>'cors'], function (){
+
+    Route::prefix('v1')
+        ->namespace('Api\V1')
+        ->group(function () {
+
+            Route::resource('/products', 'ProductsController',['except' => ['edit', 'create','show']]);
+            Route::resource('/sales', 'SaleOrdersController',['except' => ['edit', 'create','show']]);
+
+
+        });
 });
