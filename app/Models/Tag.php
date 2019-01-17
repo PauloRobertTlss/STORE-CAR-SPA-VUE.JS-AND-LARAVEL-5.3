8edc8281dbe6b865a1e76265a550d3280e8823a9
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-17 20:21:14 -0200

    timestamp false;

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -14,7 +14,7 @@ use Prettus\Repository\Traits\TransformableTrait;
 class Tag extends Model implements Transformable
 {
     use TransformableTrait;
-
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
