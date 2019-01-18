e0ccfaf4719effcb5387ebd8dbabb3ea0f736e0a
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-18 15:13:42 -0200

    adicionado component loading.

--- resources/assets/_store/js/App.vue
+++ resources/assets/_store/js/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <loading :is-loading="loading" v-show="loading"></loading>
   </div>
 </template>
 
 <script>
-  export default{
-    name: 'App'
-  }
+    import { mapGetters } from 'vuex'
+    import Loading from '_@/js/components/Loading'
+    export default{
+        name: 'App',
+        components:{Loading},
+        computed: {
+            ...mapGetters([
+                'loading'
+            ])
+        }
+    }
 </script>
