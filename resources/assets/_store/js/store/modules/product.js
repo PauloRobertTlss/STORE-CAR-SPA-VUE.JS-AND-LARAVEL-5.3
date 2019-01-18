1cbf5b874c7f6e254ab4c77f4abe404f72a11f45
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-18 10:01:31 -0200

    inserindo scroll infinit de products, pagination.

--- resources/assets/_store/js/store/modules/product.js
+++ resources/assets/_store/js/store/modules/product.js
@@ -1,12 +1,12 @@
 
-import { fetchList,getInfo} from '_@/js/api/PRODUCTS'
+import { fetchList,getInfo} from '_@/js/api/products'
 import SearchOptions from '_@/js/services/search-options';
-
+import _ from 'lodash'
 const product = {
     state: {
         IDCurrent: null,
         product: {name:`Teste`},
-        PRODUCTS: [],
+        products: [],
         searchOptions: new SearchOptions()
     },
     mutations: {
@@ -18,6 +18,11 @@ const product = {
         },
         SET_PRODUCTS: (state, data) => {
             state.products = data
+        },
+        ONION_PRODUCTS: (state, data) => {
+            console.log('befone products ['+state.products.length+']')
+            state.products = _.union(state.products, data);
+            console.log('after products ['+state.products.length+']')
         },
         SET_PRODUCT_FILTER: (state, filter) => {
             state.searchOptions.search = filter;
@@ -36,6 +41,12 @@ const product = {
         SET_PRODUCT_CURRENT_PAGE: (state,currentPage) => {
             state.searchOptions.pagination.current_page = currentPage;
         },
+        SET_PRODUCT_NEXT_PAGE: (state) => {
+            if(state.products.length < state.searchOptions.pagination.total) {
+                state.searchOptions.pagination.current_page = (state.searchOptions.pagination.current_page + 1);
+                console.log('proxima pagina', state.searchOptions.pagination.current_page)
+            }
+        },
         SET_PRODUCT_SORT: (state,sort) => {
             state.searchOptions.order.sort = sort;
         }
@@ -52,6 +63,32 @@ const product = {
         },
         queryProducts({commit,state}) {
             let searchOptions = state.searchOptions;
+
+
+                return new Promise((resolve, reject) => {
+                    return fetchList(searchOptions.createOptions(),
+                        {
+                            //use before callback
+                            before(request) {
+                                //abort previous request, if exists
+                                if (this.previousRequest) {
+                                    this.previousRequest.abort();
+                                }
+                                //set previous request on Vue instance
+                                this.previousRequest = request;
+                            }
+                        }
+                    ).then(response => {
+                        commit('SET_PRODUCTS', response.data.data);
+                        commit('SET_PRODUCT_PAGINATION', response.data.meta.pagination);
+                        resolve(response);
+                    });
+                });
+
+        },
+        unionProducts({commit,state}) {
+            let searchOptions = state.searchOptions;
+            if(state.products.length < state.searchOptions.pagination.total) {
             return new Promise((resolve, reject) => {
                 return fetchList(searchOptions.createOptions(),
                     {
@@ -65,10 +102,14 @@ const product = {
                             this.previousRequest = request;
                         }}
                 ).then(response => {
-                    commit('SET_PRODUCTS', response.data.data);
+
+                    commit('ONION_PRODUCTS', response.data.data);
+                    commit('SET_PRODUCT_PAGINATION', response.data.meta.pagination);
                     resolve(response);
                 });
             });
+            }
+            console.log('já carregamos todos os produtos desta pagina' ,state.searchOptions.pagination.current_page)
         }
     }
 };
