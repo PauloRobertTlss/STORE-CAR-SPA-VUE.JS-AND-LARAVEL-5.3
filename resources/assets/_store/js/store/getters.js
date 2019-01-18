bbcdef9d3c2ee90bcc3c63c293123f3a5d46dba5
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-18 15:12:40 -0200

    adicionado getter loading

--- resources/assets/_store/js/store/getters.js
+++ resources/assets/_store/js/store/getters.js
@@ -1,4 +1,5 @@
 const getters = {
+    loading: state => state.app.loading,
     sidebar: state => state.app.sidebar,
     language: state => state.app.language,
     device: state => state.app.device,
