1570cded225060c32bbf6a37aed07b5ef66a23ba
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-21 20:03:54 -0200

    Refatorado metodo index. Será enviado como params a categia para consultas.

--- app/Http/Controllers/Api/V1/ProductsController.php
+++ app/Http/Controllers/Api/V1/ProductsController.php
@@ -3,6 +3,7 @@
 namespace StoreTI\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
+use StoreTI\Criteria\FindByColumnEqualCriteria;
 use StoreTI\Http\Controllers\Controller;
 use Prettus\Repository\Criteria\RequestCriteria;
 use StoreTI\Presenters\ProductDetailsPresenter;
@@ -23,11 +24,26 @@ class ProductsController extends Controller
     
     public function index(Request $request)
     {
-
         $limit = $request->query->get('limit',10);
-        $searchParam = config('repository.criteria.params.search');
 
-        $this->productRepository->pushCriteria(app(RequestCriteria::class));
+        $searchParam = config('repository.criteria.params.search');
+        $search = $request->get($searchParam);
+        $field = explode('-',$search);
+        $this->productRepository->popCriteria(RequestCriteria::class);
+        $this->productRepository->popCriteria(FindByColumnEqualCriteria::class);
+
+        //return response()->json($field,422);
+        switch (count($field)){
+            case 2:{
+                list($field,$search_id) = $field;
+                $this->productRepository->pushCriteria(new FindByColumnEqualCriteria($search_id, $field));
+                break;
+            }
+            default:
+                $this->productRepository->pushCriteria(app(RequestCriteria::class));
+        }
+
+
         return $this->productRepository->paginate($limit);
     }
     
