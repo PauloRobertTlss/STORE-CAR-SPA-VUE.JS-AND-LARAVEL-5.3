d44e57314118b77e82602ba4199f193af8b2ce05
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-21 14:16:01 -0200

    validadores de Emails;NumberPhone

---
+++ resources/assets/_store/js/utils/validate.js
@@ -0,0 +1,23 @@
+/**
+ * validate email
+ * @param email
+ * @returns {boolean}
+ */
+export function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email)
+}
+
+
+/**
+ * validate Mobile Number
+ * @param number
+ * @returns {boolean}
+ */
+export function validateMobileNumber(number) {
+  let prefix = number.slice(2, 3);
+  return prefix == 9;
+}
+
+
+
