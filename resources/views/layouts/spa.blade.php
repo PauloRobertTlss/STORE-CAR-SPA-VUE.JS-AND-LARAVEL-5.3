bbd8dccc59b193799a47089b0d68404dbe959a87
PauloRobert <pauloblacksheep@hotmail.com>
2019-01-22 12:49:01 -0200

    definindo arquivos/ambientes de produção e desenvolvimento.

--- resources/views/layouts/spa.blade.php
+++ resources/views/layouts/spa.blade.php
@@ -12,14 +12,25 @@
     </script>
 </head>
 <body>
+
+
 @if (getenv('APP_ENV') === 'local')
     <script id="__bs_script__">//<![CDATA[
         document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.6'><\/script>".replace("HOST", location.hostname));
         //]]>
     </script>
+    @else
 @endif
+
 <div id="app-container"></div>
 <!-- Scripts -->
-<script src="{{asset('build/store.bundle.js')}}"></script>
+@if (getenv('APP_ENV') === 'local')
+    <script src="{{asset('build/spa.bundle.js')}}"></script>
+@else
+    <script src="{{asset('build/js/manifest.prod.js')}}"></script>
+    <script src="{{asset('build/js/vendor.prod.js')}}"></script>
+    <script src="{{asset('build/js/spa.prod.js')}}"></script>
+@endif
+
 </body>
 </html>
